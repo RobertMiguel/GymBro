fabae0805a6072a8c602b7daf325be881c69bf89
RobertMiguel <robertresende9@gmail.com>
2023-07-31 23:30:03 -0300

    more erros

--- src/routes/AppDrawer.tsx
+++ src/routes/AppDrawer.tsx
@@ -14,10 +14,6 @@ export function AppDrawer() {
         name='Meu Perfil'
         component={MyProfileScreen}
       />
-      <Drawer.Screen
-        name='Configurações'
-        component={}
-      />
     </Drawer.Navigator>
   )
 }
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 
 import { AppDrawer } from './AppDrawer'
+import { AppTabBar } from './AppTabBar'
 
 export function Router() {
   return (
     <NavigationContainer>
-      <AppDrawer />
+      <AppTabBar />
     </NavigationContainer>
   )
 }
